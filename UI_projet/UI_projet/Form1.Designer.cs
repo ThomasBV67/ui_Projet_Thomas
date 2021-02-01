ea3a60a9334434a3fd1c7cb1a52ae73ed9ead9a4
Thomas <thomasbureauviens@gmail.com>
2021-01-31 22:09:00 -0500

    d

--- UI_projet/UI_projet/Form1.Designer.cs
+++ UI_projet/UI_projet/Form1.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnCurrentTemp = new System.Windows.Forms.Button();
             this.panelSelectSection = new System.Windows.Forms.FlowLayoutPanel();
             this.btnTempHistory = new System.Windows.Forms.Button();
@@ -35,11 +38,19 @@
             this.btnInfo = new System.Windows.Forms.Button();
             this.tabControlMain = new System.Windows.Forms.TabControl();
             this.tabCurrentTemp = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabHistory = new System.Windows.Forms.TabPage();
             this.tabAddCapt = new System.Windows.Forms.TabPage();
             this.tabInfo = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panelSelectSection.SuspendLayout();
             this.tabControlMain.SuspendLayout();
+            this.tabCurrentTemp.SuspendLayout();
+            this.tabHistory.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCurrentTemp
@@ -107,6 +118,10 @@
             // 
             // tabCurrentTemp
             // 
+            this.tabCurrentTemp.Controls.Add(this.textBox1);
+            this.tabCurrentTemp.Controls.Add(this.button2);
+            this.tabCurrentTemp.Controls.Add(this.tb);
+            this.tabCurrentTemp.Controls.Add(this.button1);
             this.tabCurrentTemp.Location = new System.Drawing.Point(4, 22);
             this.tabCurrentTemp.Name = "tabCurrentTemp";
             this.tabCurrentTemp.Padding = new System.Windows.Forms.Padding(3);
@@ -115,8 +130,46 @@
             this.tabCurrentTemp.Text = "Températures actuelles";
             this.tabCurrentTemp.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(237, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 153);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Capteur : \r\nChambre Froide 3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 263);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Capteur précédent";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tb
+            // 
+            this.tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb.Location = new System.Drawing.Point(400, 349);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(204, 80);
+            this.tb.TabIndex = 1;
+            this.tb.Text = "3.4 oC";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 263);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prochain capteur";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // tabHistory
             // 
+            this.tabHistory.Controls.Add(this.chart1);
             this.tabHistory.Location = new System.Drawing.Point(4, 22);
             this.tabHistory.Name = "tabHistory";
             this.tabHistory.Padding = new System.Windows.Forms.Padding(3);
@@ -143,6 +196,22 @@
             this.tabInfo.Text = "Information";
             this.tabInfo.UseVisualStyleBackColor = true;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 6);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(891, 533);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -154,6 +223,10 @@
             this.Text = "Form1";
             this.panelSelectSection.ResumeLayout(false);
             this.tabControlMain.ResumeLayout(false);
+            this.tabCurrentTemp.ResumeLayout(false);
+            this.tabCurrentTemp.PerformLayout();
+            this.tabHistory.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -170,6 +243,11 @@
         private System.Windows.Forms.TabPage tabHistory;
         private System.Windows.Forms.TabPage tabAddCapt;
         private System.Windows.Forms.TabPage tabInfo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
